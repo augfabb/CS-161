2303635d3b40381e2b72d222598fe6e95a3fad5f
augfabb <augdogwww@gmail.com>
2025-09-25 21:40:17 +0000

    yay

---
+++ assignment1.cpp
@@ -0,0 +1,84 @@
+// ------------- FILE HEADER -------------
+// Author ✅: 
+// Assignment ✅:
+// Date ✅:
+// Citations: 
+
+
+// ------------- ZYBOOKS SCORES -------------
+// Chapter ✅: 
+// Participation ✅: 
+// Challenge ✅:
+// Labs ✅:
+
+
+// ------------- DISCORD POSTS -------------
+// https://discord.com/invite/URYKKf8YHm
+// Count ✅:
+// Links (Optional): 
+
+
+// ------------- DESIGN DOCUMENT -------------
+// A. INPUT ✅: 
+// B. OUTPUT ✅:
+// C. CALCULATIONS ✅:
+// D. LOGIC and ALGORITHMS ✅:
+//    (Optional) flow chart link or file name: 
+
+
+// ------------- TESTING -------------
+// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
+// (Optional) Additional tests count:   
+
+
+// ------------- CODE -------------
+#include <iostream>
+
+using namespace std;
+
+// Function prototypes (if any)
+
+
+// Main function
+// https://en.cppreference.com/w/cpp/language/main_function.html
+int main(int argc, char* argv[]) {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
+
+// Function implementations (if any)
+
+
+// ------------- DESIGN -------------
+/* 
+Program Name:
+
+Program Description:
+
+Design:
+A. INPUT
+int employee_id = 0;
+int hours_worked = 0;
+int usd_per_hour = 0;
+int federal_withholding_percent = 0;
+
+
+B. OUTPUT
+Define the output variables including data types. 
+
+C. CALCULATIONS
+Describe calculations used by algorithms in step D.  
+List all formulas. 
+If there are no calculations needed, state there are no calculations.
+
+D. LOGIC and ALGORITHMS
+Design the logic of your program using pseudocode or flowcharts. 
+Use conditionals, loops, functions or array constructs.
+List the steps in transforming inputs into outputs. 
+https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+
+
+SAMPLE RUNS
+Copy from assignment document.
+
+*/
